30d21397b1e10fc8fce7777db2e192b59d6041f1
Your Name <Inam_Ullah@ecommercesteem.com>
2026-09-09 00:45:56 +0500

    feat(api): implement product routes and controller with data seeding functionality

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use('/api/products', productRoutes);
 
 app.use(cors());
 app.use(express.json());
